35d6893fe5a89c75a3230e1db55edcc752174fc6
chintanshah35 <chintanshah35@gmail.com>
2026-04-25 10:47:33 -0400

    Got file walker working

--- src/scanner/walker.ts
+++ src/scanner/walker.ts
@@ -1,14 +1,29 @@
-import { readdir } from 'fs/promises'
-import { join } from 'path'
+import { readdir, stat } from 'fs/promises'
+import { join, relative } from 'path'
 
-// TODO: add ignore support, binary detection
-export async function* walkFiles(directory: string): AsyncGenerator<string> {
+export type WalkerOptions = {
+  ignore?: string[]
+  root: string
+}
+
+const DEFAULT_IGNORE = ['node_modules', '.git', 'dist', 'build', 'coverage', '.next', '.nuxt']
+
+export async function* walkFiles(directory: string, options: WalkerOptions): AsyncGenerator<string> {
+  const ignore = [...DEFAULT_IGNORE, ...(options.ignore ?? [])]
+  yield* walk(directory, ignore, options.root)
+}
+
+async function* walk(directory: string, ignore: string[], root: string): AsyncGenerator<string> {
   const entries = await readdir(directory, { withFileTypes: true })
 
   for (const entry of entries) {
     const fullPath = join(directory, entry.name)
+    const relativePath = relative(root, fullPath)
+
+    if (ignore.some((pattern) => relativePath.includes(pattern))) continue
+
     if (entry.isDirectory()) {
-      yield* walkFiles(fullPath)
+      yield* walk(fullPath, ignore, root)
     } else if (entry.isFile()) {
       yield fullPath
     }
